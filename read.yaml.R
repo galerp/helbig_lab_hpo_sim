fef73b7a59817b856c504564563a77c2b642d92c
shiva-g <gshiva1994@gmail.com>
2018-11-20 16:53:58 -0500

    Update read.yaml.R

--- read.yaml.R
+++ read.yaml.R
@@ -1,10 +1,11 @@
 
 if(file.exists("input.yml") == T){
   input.yaml <- yaml::read_yaml("input.yml")
-}else{
+} else {
   message('Input YAML not found.\n')
   break;
 }
+
 options(stringsAsFactors = F)
 
 setwd(input.yaml$working_dir)
@@ -20,19 +21,25 @@ variant <- read_csv(input.yaml$variant_file)
 
 
 if( is.null(input.yaml$local_IC)) { 
+
     source("master.R")
+
 } else if(is.null(input.yaml$sim_matrix)) {
   
   local_IC <- read_csv(input.yaml$local_IC)
   pat_table_base <- read_csv(input.yaml$patient_hpo_base)
   pat_table_prop <- read_csv(input.yaml$patient_hpo_prop)
+  
   source("precalc_IC.R")
-  } else {
-    local_IC <- read_csv(input.yaml$local_IC)
+  
+} else {
+
+  local_IC <- read_csv(input.yaml$local_IC)
     pat_table_base <- read_csv(input.yaml$patient_hpo_base)
     pat_table_prop <- read_csv(input.yaml$patient_hpo_prop)
     sim_score <- read_csv(input.yaml$sim_matrix)
     source("precalc_sim_matrix.R")
-  }
+  
+}
 
 
